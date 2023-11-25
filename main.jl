e7f725d9a5b8881984ced623a54a68b6f0b4316b
Jan Z <jan.znz@hotmail.com>
2023-11-25 21:58:41 +0100

    Solver_z korrigiert
    Es besteht ein Problem mit dem Vorfaktor. Dadurch kann kein Plot mit Werten eta0 > 0.6 angefertigt für alpha = 1. Dies muss noch korrigiert werden, vorläufig kann aber mit dieser Version gearbeitet werden.

--- main.jl
+++ main.jl
@@ -10,7 +10,7 @@ function installed()
   return installs
 end
 # Check if packages are installed, else install them
-Packages = ["Plots", "Test"]
+Packages = ["Plots", "Test", "LaTeXStrings"]
 installed_Packages = keys(installed())
 for Package in Packages
   if !(Package in installed_Packages)
@@ -33,7 +33,7 @@ include("src/utils.jl")
 include("test/test_solver.jl")
 
 function test()
-  η0 = 0.1
+  η0 = 0.2
   L = 3
   M = 30 + L
   v_ext = vcat(zeros(Float64, L), ones(Float64, M-L-1), zeros(Float64, L))
@@ -59,24 +59,44 @@ end
 function test_z()
 
   # Lege Parameter und externes Potential fest
-  η0 = 0.1
+  η1 = 0.1
+  η2 = 0.2
+  η3 = 0.3
+  η4 = 0.4
+  η5 = 0.5
+  η6 = 0.6
+  η7 = 0.7
+  η8 = 0.8
+  η9 = 0.9
   L  = 3
   M  = 30 + L
   α  = 1.0
   v_ext = vcat(zeros(Float64, L), ones(Float64, M-L-1), zeros(Float64, L))
 
   # Generiere System zum testen
-  it_0 = make_RodLat(L, M, η0, v_ext)
-  #println(it_0)
+  sys1 = make_RodLat(L, M, η1, v_ext)
+  sys2 = make_RodLat(L, M, η2, v_ext)
+  sys3 = make_RodLat(L, M, η3, v_ext)
+  sys4 = make_RodLat(L, M, η4, v_ext)
+  sys5 = make_RodLat(L, M, η5, v_ext)
+  sys6 = make_RodLat(L, M, η6, v_ext)
+  #sys7 = make_RodLat(L, M, η7, v_ext)
+  #sys8 = make_RodLat(L, M, η8, v_ext)
+  #sys9 = make_RodLat(L, M, η9, v_ext)
 
-  # Testsystem nach 1 Iteration
-  it_1 = ρ_step(it_0, α)
-  it_2 = ρ_step(deepcopy(it_1), α)
-  it_3 = ρ_step(deepcopy(it_2), α)
-  it_4 = ρ_step(deepcopy(it_3), α)
+  # Generiere Entwicklung bzgl. verschiedener η
+  A1 = ρ_step(sys1, α)
+  A2 = ρ_step(sys2, α)
+  A3 = ρ_step(sys3, α)
+  A4 = ρ_step(sys4, α)
+  A5 = ρ_step(sys5, α)
+  A6 = ρ_step(sys6, α)
+  #A7 = ρ_step(sys7, α)
+  #A8 = ρ_step(sys8, α)
+  #A9 = ρ_step(sys9, α)
 
-  plot(eachindex(it_1.ρ) |> collect, [it_1.ρ, it_2.ρ, it_3.ρ, it_4.ρ], label=permutedims(string.(0:3|>collect) .* " steps"), legend=:bottom)
-  vline!([L, M], label="Wall")
+  plot((eachindex(A1.ρ) |> collect)[3:30], [A1.ρ[3:30], A2.ρ[3:30], A3.ρ[3:30], A4.ρ[3:30], A5.ρ[3:30], A6.ρ[3:30]], label=[L"\eta_0 = 0.1" L"\eta_0 = 0.2" L"\eta_0 = 0.3" L"\eta_0 = 0.4" L"\eta_0 = 0.5" L"\eta_0 = 0.6"], xlabel = "s", ylabel = L"\rho", title = "L = 3", legend = :topright, dpi = 300)
+  #plot((eachindex(A1.ρ) |> collect)[3:30], [A1.ρ[3:30], A2.ρ[3:30], A3.ρ[3:30], A4.ρ[3:30], A5.ρ[3:30], A6.ρ[3:30], A7.ρ[3:30], A8.ρ[3:30], A9.ρ[3:30]], label=[L"\eta_0 = 0.1" L"\eta_0 = 0.2" L"\eta_0 = 0.3" L"\eta_0 = 0.4" L"\eta_0 = 0.5" L"\eta_0 = 0.6" L"\eta_0 = 0.7" L"\eta_0 = 0.8" L"\eta_0 = 0.9"], xlabel = "s", ylabel = L"\rho", title = "L = 3", legend = :topright, dpi = 300)
 end
 
 test_z()
--- src/solver/solver_z.jl
+++ src/solver/solver_z.jl
@@ -14,29 +14,46 @@ end
 
 # Berechne µ_ex
 function μ_ex(s::Integer, L::Integer, ρs::Vector{Float64})
-   len_ρs = size(ρs, 1)
-   µs1 = 0
-   µs2 = 0
+
+    res = 0
 
    # Berechne µs1
-   for i in (s-L+1):s
-    µs1 += -log(1-n((i-L+1), i, ρs))
+   for i in s:(s+L-1)
+    res += -log(1-n((i-L+1), i, ρs))
    end
 
    # Berechne µs2
-   for i in (s-L+11):(s-1)
-    println("prob hier")
-    -log(1-n((i-L+1), (i-1), ρs))
+   for i in (s+1):(s+L-1)
+    res -= -log(1-n((i-L+1), (i-1), ρs))
    end
+   
+   return res
+   
+end
 
-   # Gebe µ_ex zurück
-   return µs1 - µs2 
+
+# Alternativer Weg für die Berechnung von µ_ex
+
+function μ_ex_alternative(s::Integer, L::Integer, ρs::Vector{Float64})
+    
+    res = -log(1 - n((s-L+1), s, ρs))
+
+    # Berechne Summe
+    for i in (s+1):(s+L-1)
+        res += log((1 - n((i-L+1), (i-1), ρs)) / (1 - n((i-L+1), i, ρs)))
+    end
+
+    return res
+
 end
 
 
 # Berechne den Vorfaktor für ρs
 function prefactor(L::Integer, ρ0::Float64)
-    return ρ0 * exp(L * log(1 + (ρ0/(1 - L * ρ0))) - log(1 + ρ0 + L * ρ0))
+
+    return ρ0 * exp(L * log(1 + (ρ0/ (1 - L * ρ0))) - log(1 + ρ0 - L * ρ0))
+    #return (η0 / L) * exp(L * log(1 + (η0/ (L * (1 - η0)))) - log(1 + (η0 / L) - η0))
+
 end
 
 
@@ -44,11 +61,9 @@ function ρ_step(sys::RodLat, α::Float64)
     
     # Ziehe Systemdaten
     L     = sys.L
-    M     = sys.M
     ρ0    = sys.ρ0
     v_ext = sys.v_ext
     ρs    = sys.ρ
-    test=deepcopy(ρs)
 
     # Ziehe Parameter für Berechnung der neuen Dichte-Werte nach Picard & mixing
     len_ρs = size(ρs, 1)
@@ -56,15 +71,16 @@ function ρ_step(sys::RodLat, α::Float64)
 
     # Berechne für jeden index s in ρs den neuen Wert 
     for s in 1:len_ρs
-        value    = prefactor(L, ρ0) * exp(-1 * μ_ex(s, L, ρs)) * v_ext[s]
-        ρs_new[s] = (1 - α) * ρs[s] + α * value
+        value     = prefactor(L, ρ0) * exp(-1 * μ_ex(s, L, ρs_new)) * v_ext[s]
+        ρs_new[s] = (1 - α) * ρs_new[s] + α * value
     end
 
     # Update System
     sys.ρ = ρs_new
-
-    println(test .- ρs_new)
+    sys.ρ0 = sys.ρ[div(end, 2)]
+    sys.η0 = sys.ρ0 * sys.L
 
     # Gebe System zurück
     return sys
+
 end
