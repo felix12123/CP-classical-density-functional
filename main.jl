7c1a1d28d8030669fafa47c7c82ce75b1edfa8ad
Jan Z <jan.znz@hotmail.com>
2023-11-28 21:35:37 +0100

    Code erweitert um rho_steps
    Es wird nicht nur eine Iteration berechnet, sondern es wird das Konvergenzkriterium angewendet.
    
    Ausstehend
    -------------
    
    - Code kompakter machen
    -  Surface-Tension usw.

--- main.jl
+++ main.jl
@@ -68,35 +68,50 @@ function test_z()
   η7 = 0.7
   η8 = 0.8
   η9 = 0.9
-  L  = 3
-  M  = 30 + L
-  α  = 1.0
+  #η = 0.1:0.1:0.9
+  L  = 10
+  M  = 500 + L
+  α  = 0.001
+  ϵ  = 1e-10
+  ϵ8 = 1e-9     # FIXME: Wieso ist das nicht ~10e-10?
+  ϵ9 = 1e-5     # FIXME: Wieso ist das nicht ~10e-10?
   v_ext = vcat(zeros(Float64, L), ones(Float64, M-L-1), zeros(Float64, L))
 
-  # Generiere System zum testen
+  # Generiere Systeme
   sys1 = make_RodLat(L, M, η1, v_ext)
   sys2 = make_RodLat(L, M, η2, v_ext)
   sys3 = make_RodLat(L, M, η3, v_ext)
   sys4 = make_RodLat(L, M, η4, v_ext)
   sys5 = make_RodLat(L, M, η5, v_ext)
   sys6 = make_RodLat(L, M, η6, v_ext)
-  #sys7 = make_RodLat(L, M, η7, v_ext)
-  #sys8 = make_RodLat(L, M, η8, v_ext)
-  #sys9 = make_RodLat(L, M, η9, v_ext)
+  sys7 = make_RodLat(L, M, η7, v_ext)
+  sys8 = make_RodLat(L, M, η8, v_ext)
+  sys9 = make_RodLat(L, M, η9, v_ext)
 
-  # Generiere Entwicklung bzgl. verschiedener η
-  A1 = ρ_step(sys1, α)
-  A2 = ρ_step(sys2, α)
-  A3 = ρ_step(sys3, α)
-  A4 = ρ_step(sys4, α)
-  A5 = ρ_step(sys5, α)
-  A6 = ρ_step(sys6, α)
-  #A7 = ρ_step(sys7, α)
-  #A8 = ρ_step(sys8, α)
-  #A9 = ρ_step(sys9, α)
+  println("Stats for Sys1")
+  sys1 = ρ_steps(sys1, α, ϵ)
+  println("\n Stats for Sys2")
+  sys2 = ρ_steps(sys2, α, ϵ)
+  println("\n Stats for Sys3")
+  sys3 = ρ_steps(sys3, α, ϵ)
+  println("\n Stats for Sys4")
+  sys4 = ρ_steps(sys4, α, ϵ)
+  println("\n Stats for Sys5")
+  sys5 = ρ_steps(sys5, α, ϵ)
+  println("\n Stats for Sys6")
+  sys6 = ρ_steps(sys6, α, ϵ)
+  println("\n Stats for Sys7")
+  sys7 = ρ_steps(sys7, α, ϵ)
+  println("\n Stats for Sys8")
+  sys8 = ρ_steps(sys8, α, ϵ8)
+  println("\n Stats for Sys9")
+  sys9 = ρ_steps(sys9, α, ϵ9)
+  println("\n")
 
-  plot((eachindex(A1.ρ) |> collect)[3:30], [A1.ρ[3:30], A2.ρ[3:30], A3.ρ[3:30], A4.ρ[3:30], A5.ρ[3:30], A6.ρ[3:30]], label=[L"\eta_0 = 0.1" L"\eta_0 = 0.2" L"\eta_0 = 0.3" L"\eta_0 = 0.4" L"\eta_0 = 0.5" L"\eta_0 = 0.6"], xlabel = "s", ylabel = L"\rho", title = "L = 3", legend = :topright, dpi = 300)
-  #plot((eachindex(A1.ρ) |> collect)[3:30], [A1.ρ[3:30], A2.ρ[3:30], A3.ρ[3:30], A4.ρ[3:30], A5.ρ[3:30], A6.ρ[3:30], A7.ρ[3:30], A8.ρ[3:30], A9.ρ[3:30]], label=[L"\eta_0 = 0.1" L"\eta_0 = 0.2" L"\eta_0 = 0.3" L"\eta_0 = 0.4" L"\eta_0 = 0.5" L"\eta_0 = 0.6" L"\eta_0 = 0.7" L"\eta_0 = 0.8" L"\eta_0 = 0.9"], xlabel = "s", ylabel = L"\rho", title = "L = 3", legend = :topright, dpi = 300)
+  #plot((eachindex(sys1.ρ) |> collect)[3:30], [sys1.ρ[3:30], sys2.ρ[3:30], sys3.ρ[3:30], sys4.ρ[3:30], sys5.ρ[3:30], sys6.ρ[3:30], sys7.ρ[3:30], sys8.ρ[3:30], sys9.ρ[3:30]], label=[L"\eta_0 = 0.1" L"\eta_0 = 0.2" L"\eta_0 = 0.3" L"\eta_0 = 0.4" L"\eta_0 = 0.5" L"\eta_0 = 0.6" L"\eta_0 = 0.7" L"\eta_0 = 0.8" L"\eta_0 = 0.9"], xlabel = "s", ylabel = L"\rho", title = "L = 3", legend = :outerright, dpi = 300)
+  #savefig("L=3_Dichteprofil.png")
+  plot((eachindex(sys1.ρ) |> collect)[10:100], [sys1.ρ[10:100], sys2.ρ[10:100], sys3.ρ[10:100], sys4.ρ[10:100], sys5.ρ[10:100], sys6.ρ[10:100], sys7.ρ[10:100], sys8.ρ[10:100], sys9.ρ[10:100]], label=[L"\eta_0 = 0.1" L"\eta_0 = 0.2" L"\eta_0 = 0.3" L"\eta_0 = 0.4" L"\eta_0 = 0.5" L"\eta_0 = 0.6" L"\eta_0 = 0.7" L"\eta_0 = 0.8" L"\eta_0 = 0.9"], xlabel = "s", ylabel = L"\rho", title = "L = 10", legend = :outerright, dpi = 300)
+  savefig("L=10_Dichteprofil.png")
 end
 
 test_z()
--- src/solver/solver_z.jl
+++ src/solver/solver_z.jl
@@ -1,7 +1,7 @@
 # Berechnung der n_s(0) bzw. n_(1)
 function n(start::Integer, stop::Integer, ρs::Vector{Float64})
     
-    len_ρs =size(ρs, 1)
+    len_ρs = size(ρs, 1)
 
     sum = 0
     for i in max(1, start):min(len_ρs, stop)
@@ -32,29 +32,14 @@ function μ_ex(s::Integer, L::Integer, ρs::Vector{Float64})
 end
 
 
-# Alternativer Weg für die Berechnung von µ_ex
-
-function μ_ex_alternative(s::Integer, L::Integer, ρs::Vector{Float64})
-    
-    res = -log(1 - n((s-L+1), s, ρs))
-
-    # Berechne Summe
-    for i in (s+1):(s+L-1)
-        res += log((1 - n((i-L+1), (i-1), ρs)) / (1 - n((i-L+1), i, ρs)))
-    end
-
-    return res
-
-end
-
 
 # Berechne den Vorfaktor für ρs
 function prefactor(L::Integer, ρ0::Float64)
 
     return ρ0 * exp(L * log(1 + (ρ0/ (1 - L * ρ0))) - log(1 + ρ0 - L * ρ0))
-    #return (η0 / L) * exp(L * log(1 + (η0/ (L * (1 - η0)))) - log(1 + (η0 / L) - η0))
 
 end
+
 
 
 function ρ_step(sys::RodLat, α::Float64)
@@ -66,21 +51,64 @@ function ρ_step(sys::RodLat, α::Float64)
     ρs    = sys.ρ
 
     # Ziehe Parameter für Berechnung der neuen Dichte-Werte nach Picard & mixing
-    len_ρs = size(ρs, 1)
-    ρs_new = deepcopy(ρs)
+    len_ρs            = size(ρs, 1)
+    ρs_i1             = deepcopy(ρs)
+    convergence_array = ones(Float64, len_ρs)
 
     # Berechne für jeden index s in ρs den neuen Wert 
     for s in 1:len_ρs
-        value     = prefactor(L, ρ0) * exp(-1 * μ_ex(s, L, ρs_new)) * v_ext[s]
-        ρs_new[s] = (1 - α) * ρs_new[s] + α * value
+        ρs_new_i = prefactor(L, ρ0) * exp(-1 * μ_ex(s, L, ρs)) * v_ext[s]
+        convergence_array[s] = (ρs_new_i - ρs[s])^2
+        ρs_i1[s] = (1 - α) * ρs[s] + α * ρs_new_i
     end
 
     # Update System
-    sys.ρ = ρs_new
+    sys.ρ = ρs_i1
     sys.ρ0 = sys.ρ[div(end, 2)]
     sys.η0 = sys.ρ0 * sys.L
 
-    # Gebe System zurück
+    # Gebe System aus
+    return sys
+
+end
+
+
+
+function ρ_steps(sys::RodLat, α::Float64, ϵ::Float64)
+
+    convergent = false
+    convergence_array = ones(Float64, size(sys.ρ, 1))
+    counter = 1
+
+    while !convergent
+        counter += 1
+        # Ziehe wichtige Daten
+        ρ0                = sys.ρ0
+        v_ext             = sys.v_ext
+        ρs                = sys.ρ
+        len_ρs            = size(ρs, 1)
+        L                 = sys.L
+        ρs_i1             = deepcopy(ρs)
+
+        # Berechne für jeden index s in ρs den neuen Wert 
+       for s in 1:len_ρs
+           ρs_new_i = prefactor(L, ρ0) * exp(-1 * μ_ex(s, L, ρs)) * v_ext[s]
+           convergence_array[s] = (ρs_new_i - ρs[s])^2
+           ρs_i1[s] = (1 - α) * ρs[s] + α * ρs_new_i
+       end
+
+        # Update System
+        if sum(convergence_array) < ϵ
+            convergent = true
+            println("Konvergenz-Summe = ", sum(convergence_array))
+            println("Iterationen bis zur Konvergenz: ", counter)
+        end
+        sys.ρ = ρs_i1
+        sys.ρ0 = sys.ρ[div(end, 2)]
+        sys.η0 = sys.ρ0 * sys.L
+    end
+
+    # Gebe System aus
     return sys
 
 end
